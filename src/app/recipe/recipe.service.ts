f8368abd0a1013dec6392a28c97520f6fe475879
krantikumargajula <kranti.gajula@skarma.com>
2017-03-16 18:55:46 +0530

    Done as per tutuorial but with issue
    Not able to see to tabs

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe';
+import { Ingredient } from '../ingredient';
 
 @Injectable()
 export class RecipeService {
 
 	private recipes: Recipe[] = [
-      new Recipe('Recipe1', 'Dummy Recipe', 'http://www.bbcgoodfood.com/sites/default/files/recipe_images/recipe-image-legacy-id--364143_11.jpg',[]),
+      new Recipe('Recipe1', 'Dummy Recipe', 'http://www.bbcgoodfood.com/sites/default/files/recipe_images/recipe-image-legacy-id--364143_11.jpg',[
+      	new Ingredient('water', 1),
+      	new Ingredient('sugar', 2)
+      	]),
       new Recipe('Recipe2', 'Dummy Recipe', 'http://4.bp.blogspot.com/-GOEQMgP48vY/UxgMmCf6j_I/AAAAAAAATH8/6CWDvxRqIbM/s1600/one-pot-cheesy-pasta-and-sausage-2014FB-wide2.jpg',[])
     ];
 
