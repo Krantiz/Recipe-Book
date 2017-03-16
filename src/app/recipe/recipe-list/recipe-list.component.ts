5fc656554bf00fc3a1fea9d43a9b591ed4389195
krantikumargajula <kranti.gajula@skarma.com>
2017-03-16 15:33:41 +0530

    Multiple recipes added using durectives

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -9,9 +9,13 @@ import { RecipeItemsComponent } from './recipe-items.component';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-    recipes: Recipe[] = [];
+    recipes: Recipe[] = [
+      new Recipe('Recipe1', 'Dummy Recipe', 'http://www.bbcgoodfood.com/sites/default/files/recipe_images/recipe-image-legacy-id--364143_11.jpg',[]),
+      new Recipe('Recipe2', 'Dummy Recipe', 'http://4.bp.blogspot.com/-GOEQMgP48vY/UxgMmCf6j_I/AAAAAAAATH8/6CWDvxRqIbM/s1600/one-pot-cheesy-pasta-and-sausage-2014FB-wide2.jpg',[])
+    ];
     @Output() recipeSelected = new EventEmitter<Recipe>();
-    recipe = new Recipe('Dummy', 'Dummy', 'http://www.bbcgoodfood.com/sites/default/files/recipe_images/recipe-image-legacy-id--364143_11.jpg')
+
+    // recipe = new Recipe('Dummy', 'Dummy', 'http://www.bbcgoodfood.com/sites/default/files/recipe_images/recipe-image-legacy-id--364143_11.jpg')
 
   constructor() { }
 
