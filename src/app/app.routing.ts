1c425026da06e8fc7acc6a7da9c588fc5aca3779
Krantiz <kranti.gajula@gmail.com>
2017-04-11 11:52:53 +0530

    Basic routing setup

---
+++ src/app/app.routing.ts
@@ -0,0 +1,13 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+
+const APP_ROUTES: Routes = [
+	{ path: '', redirectTo: '/recipes', pathMatch: 'full' },
+	{ path: 'recipes', component: RecipeComponent },
+	{ path: 'shopping-list', component: ShoppingListComponent }
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
